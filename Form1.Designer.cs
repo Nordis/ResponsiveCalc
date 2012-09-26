8791d2692b8c2c75e8182c8c0ac977c786efface
Daniel Nordmark <nordmark.daniel@gmail.com>
2012-09-26 10:44:03 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ResponsiveCalc
+{
+    partial class ResponsiveCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResponsiveCalc));
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstHistory = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFirst.Location = new System.Drawing.Point(12, 29);
+            this.txtFirst.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(142, 22);
+            this.txtFirst.TabIndex = 0;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSecond.Location = new System.Drawing.Point(183, 29);
+            this.txtSecond.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(142, 22);
+            this.txtSecond.TabIndex = 1;
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnEquals.Location = new System.Drawing.Point(332, 28);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(34, 23);
+            this.btnEquals.TabIndex = 2;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(161, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "/";
+            // 
+            // lstHistory
+            // 
+            this.lstHistory.FormattingEnabled = true;
+            this.lstHistory.ItemHeight = 16;
+            this.lstHistory.Location = new System.Drawing.Point(12, 58);
+            this.lstHistory.Name = "lstHistory";
+            this.lstHistory.Size = new System.Drawing.Size(313, 180);
+            this.lstHistory.TabIndex = 4;
+            this.lstHistory.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstHistory_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Target";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Context";
+            // 
+            // ResponsiveCalc
+            // 
+            this.AcceptButton = this.btnEquals;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 252);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstHistory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtFirst);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "ResponsiveCalc";
+            this.ShowIcon = false;
+            this.Text = "ResponsiveCalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstHistory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ResponsiveCalc
+{
+    public partial class ResponsiveCalc : Form
+    {
+        public ResponsiveCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            CountResponsive();
+        }
+
+        private void CountResponsive()
+        {
+            try
+            {
+                var firstNumber = Decimal.Parse(txtFirst.Text);
+                var secondNumber = Decimal.Parse(txtSecond.Text);
+
+                var responsiveRatio = string.Format("{0}%", (firstNumber / secondNumber) * 100).Replace(",", ".");
+
+                Clipboard.SetText(string.Format("{0}; /* {1}px / {2}px */", responsiveRatio, firstNumber, secondNumber));
+                //MessageBox.Show(responsiveRatio);
+
+                lstHistory.Items.Insert(0, (string.Format("{0} / {1} = {2}", firstNumber, secondNumber, responsiveRatio)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lstHistory_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int clickedIndex = lstHistory.IndexFromPoint(e.Location);
+            if (clickedIndex != ListBox.NoMatches)
+            {
+                var item = lstHistory.Items[clickedIndex].ToString();
+
+                var copyText = item.Substring(item.IndexOf("=", StringComparison.Ordinal) + 1).Trim();
+                Clipboard.SetText(copyText);
+            }
+        }
+    }
+}
